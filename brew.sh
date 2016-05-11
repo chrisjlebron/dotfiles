b99afd59e548093d569930ceead02c299d1d58ba
Chris J. Lebron <chris@chrisjlebron.com>
2016-05-11 13:17:52 -0400

    Include updates to brew.sh from master

--- brew.sh
+++ brew.sh
@@ -22,20 +22,21 @@ brew install gnu-sed --default-names
 # Bash 4
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
 brew install bash
-# regular bash-completion package is held back to an older release, so we get latest from versions.
-#   github.com/Homebrew/homebrew/blob/master/Library/Formula/bash-completion.rb#L3-L4
 brew tap homebrew/versions
-brew install homebrew/versions/bash-completion2
+brew install bash-completion2
+
+brew install homebrew/completions/brew-cask-completion
 
 # generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
 brew install grc
 
 # Install wget with IRI support
-brew install wget --enable-iri
+brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 
 
@@ -70,6 +71,9 @@ brew install mtr
 
 # Install other useful binaries
 brew install ack
+brew install the_silver_searcher
+brew install fzf
+
 brew install git
 brew install imagemagick --with-webp
 brew install node # This installs `npm` too using the recommended installation method
