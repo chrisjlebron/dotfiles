f853e55b61630a195d8f326d0b7ea9cb53b70017
Chris J. Lebron <chris@chrisjlebron.com>
2016-05-11 14:51:10 -0400

    Updates to symlink script from upstream master

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -144,10 +144,10 @@ print_success() {
 
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
+# FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin .config/fish" # add in vim and the binaries
+
 # @TODO: not yet ready: I want to grab files from the atom dir & symlink them to the user .atom dir
 declare -a ATOM_FILES_TO_SYMLINK=$(find . -name "*" -not -name ".*" -not -name "README.md" | sed -e 's|./||')
-# FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
-
 declare ATOM_FILES_DIR="$HOME/Dropbox/Dev/OSX_Settings/dotfiles/atom-files"
 
 # declare -a ATOM_FILES_TO_SYMLINK=$(find $ATOM_FILES_DIR \( -name "*.cson" -o -name "*.coffee" -o -name "*.less" \) | sed -e 's|.*/||')
@@ -222,4 +222,4 @@ atom() {
 }
 
 main
-atom
+# atom
